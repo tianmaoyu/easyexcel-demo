6936bba15c40c632f17e4be93f582240ac031c9b
eric <tianmaoyu@foxmail.com>
2025-05-21 22:28:57 +0800

    mybatis 自带工具实现

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.domain;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.example.demo.annotation.CreateId;
+import com.example.demo.annotation.UpdateId;
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class User {
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createId;
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateId;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+}
